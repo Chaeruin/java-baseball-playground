5df32914c573e794d9eb61e310a3f661d11f55d4
Chaeruin <cms3604@naver.com>
2025-02-25 15:21:46 +0900

    test: ttest case set-1 요구사항 set-1 반영

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,28 @@
+package study;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.Set;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @DisplayName("Set의 size() 메소드를 활용해 크기 확인")
+    @Test
+    void testcase4() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+}
