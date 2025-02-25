16444d7af5f2a37b90fb5d79e0759944a589d77a
Chaeruin <cms3604@naver.com>
2025-02-25 15:10:05 +0900

    test: ttest case 2 요구사항 2 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -11,11 +12,20 @@ public class StringTest {
         assertThat(actual).isEqualTo("adc");
     }
 
+    @DisplayName("문자열 1,2 를 , 로 split 했을 때 분리 확인")
     @Test
     void testcase1() {
         String[] splits = "1,2".split(",");
         assertThat(splits).contains("1");
         assertThat(splits).contains("2");
         assertThat(splits).containsExactly("1", "2");
+    }
+
+
+    @DisplayName("문자열 (1,2) 값이 주어졌을 때 () 제거 후 반환 확인")
+    @Test
+    void testcase2() {
+        String result = "(1,2)".substring(1,4);
+        assertThat(result).isEqualTo("1,2");
     }
 }
