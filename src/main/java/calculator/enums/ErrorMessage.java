1355e0c343b40843ddef3fe800ab8f8f951e0f82
Chaeruin <cms3604@naver.com>
2025-02-25 16:21:23 +0900

    feat: 문자열 계산기 ErrorMessage Enum 구현

---
+++ src/main/java/calculator/enums/ErrorMessage.java
@@ -0,0 +1,17 @@
+package calculator.enums;
+
+public enum ErrorMessage {
+
+    INVALID_INPUT_NOT_INTEGER("[ERROR] 입력에 숫자가 아닌 것이 포함되어 있습니다.");
+
+
+    private String errorMessage;
+
+    ErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public String getErrorMessage() {
+        return this.errorMessage;
+    }
+}
