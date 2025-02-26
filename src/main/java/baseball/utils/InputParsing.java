a17a4b3d14bcb18611b112e3e44916f674c4f0a7
Chaeruin <cms3604@naver.com>
2025-02-26 15:06:11 +0900

    feat: number parser 구현

--- src/main/java/baseball/utils/InputParsing.java
+++ src/main/java/baseball/utils/InputParsing.java
@@ -1,4 +1,17 @@
 package baseball.utils;
 
+import baseball.domain.Numbers;
+
 public class InputParsing {
+
+    public Numbers parseNumbers(String input) {
+        if (InputValidator.isThreeNumber(input)) {
+            String[] inputs = input.split("");
+            int first = Integer.parseInt(inputs[0]);
+            int second = Integer.parseInt(inputs[1]);
+            int third = Integer.parseInt(inputs[2]);
+            return new Numbers(first, second, third);
+        }
+        return null;
+    }
 }
