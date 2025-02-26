085d139ac5c8ac50e732b178ba2b190cfaf3213e
Chaeruin <cms3604@naver.com>
2025-02-26 15:00:04 +0900

    feat: input validator 구현

--- src/main/java/baseball/utils/InputValidator.java
+++ src/main/java/baseball/utils/InputValidator.java
@@ -1,12 +1,25 @@
 package baseball.utils;
 
+import baseball.enums.ErrorMessage;
+
 public class InputValidator {
 
     public boolean isNumeric(String input) {
         for (int i = 0; i < input.length(); i++) {
-            if (!Character.isDigit(input.charAt(i))) {
-                throw new IllegalArgumentException();
-            }
+            isDigit(input, i);
+        }
+        return true;
+    }
+
+    public void isDigit(String input, int i) {
+        if (!Character.isDigit(input.charAt(i))) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_ERROR_NOT_NUMERIC.getErrorMessage());
+        }
+    }
+
+    public boolean isThreeNumber(String input) {
+        if (isNumeric(input) && input.length() != 3) {
+            throw new IllegalArgumentException(ErrorMessage.INPUT_ERROR_NOT_THREE_NUMBER.getErrorMessage());
         }
         return true;
     }
