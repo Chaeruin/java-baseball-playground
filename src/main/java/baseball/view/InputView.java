d3465bc6b36955ae68791865c685fe4dd04dbe58
Chaeruin <cms3604@naver.com>
2025-02-26 14:34:59 +0900

    feat: input view 구현

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,13 @@
+package baseball.view;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public String getNumbers() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        return scanner.nextLine();
+    }
+}
